34ba2dd1f1c28c03de676d35c630f65cf3a1dc65
Stefano <stefano.dafarra@gmail.com>
2023-04-06 13:59:08 +0200

    Using InitVar in optimization problem test

--- test/test_optimization_problem.py
+++ test/test_optimization_problem.py
@@ -18,37 +18,39 @@ from hippopt import (
 @dataclasses.dataclass
 class MyTestVar(OptimizationObject):
     variable: StorageType = default_storage_field(ContinuousVariable)
+    size: dataclasses.InitVar[int] = dataclasses.field(default=3)
 
-    def __post_init__(self):
-        self.variable = np.zeros(3)
+    def __post_init__(self, size: int = 3):
+        self.variable = np.zeros(size)
 
 
 def test_opti_solver():
+    size = 4
     problem = OptimizationProblem()
-    var = problem.generate_optimization_objects(input_structure=MyTestVar())
+    var = problem.generate_optimization_objects(input_structure=MyTestVar(size=size))
     np.random.seed(123)
-    a = 10.0 * np.random.rand(3) + 0.01
-    b = 20.0 * np.random.rand(3) - 10.0
-    c = 20.0 * np.random.rand(3) - 10.0
+    a = 10.0 * np.random.rand(size) + 0.01
+    b = 20.0 * np.random.rand(size) - 10.0
+    c = 20.0 * np.random.rand(size) - 10.0
 
     problem.add_expression(
         mode=ExpressionType.minimize,
         expression=(
             a[k] * cs.power(var.variable[k], 2) + b[k] * var.variable[k]
-            for k in range(0, 3)
+            for k in range(size)
         ),
     )
 
     problem.add_expression(
         mode=ExpressionType.subject_to,
-        expression=(var.variable[k] >= c[k] for k in range(3)),  # noqa
+        expression=(var.variable[k] >= c[k] for k in range(size)),  # noqa
     )
 
     output, cost_value = problem.solver().solve()
 
-    expected_x = np.zeros(3)
+    expected_x = np.zeros(size)
     expected_cost = 0
-    for i in range(3):
+    for i in range(size):
         expected = -b[i] / (2 * a[i])
         expected_x[i] = expected if expected >= c[i] else c[i]
         expected_cost += (
