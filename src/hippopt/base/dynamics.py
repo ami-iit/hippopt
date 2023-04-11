6b2c018c788a8a69835fb964e5a45b2cdff46a5f
Stefano <stefano.dafarra@gmail.com>
2023-04-11 15:06:24 +0200

    More implementation on the dynamics interface.

--- src/hippopt/base/dynamics.py
+++ src/hippopt/base/dynamics.py
@@ -1,89 +1,99 @@
 import abc
 import dataclasses
-from typing import Dict, List, TypeVar
+from typing import Dict, List, Tuple, TypeVar
 
 import casadi as cs
 
-TTime = TypeVar("TTime", bound="Time")
 TDynamics = TypeVar("TDynamics", bound="Dynamics")
 TDynamicsLHS = TypeVar("TDynamicsLHS", bound="DynamicsLHS")
-
-
-class Time(abc.ABC):
-    @abc.abstractmethod
-    def t(self, *args, **kwargs) -> cs.MX:
-        pass
 
 
 @dataclasses.dataclass
 class DynamicsRHS:
     _f: cs.Function = dataclasses.field(default=None)
     _names_map: Dict[str, str] = dataclasses.field(default=None)
+    _names_map_inv: Dict[str, str] = dataclasses.field(default=None)
     f: dataclasses.InitVar[cs.Function] = None
     names_map_in: dataclasses.InitVar[Dict[str, str]] = None
 
     def __post_init__(self, f: cs.Function, names_map_in: Dict[str, str]):
         """
         Create the DynamicsRHS object
-        :param f: The CasADi function describing the dynamics
-        :param names_map_in: A map describing how to switch from the names used in the function,
-         to those of the optimization objects. The order is irrelevant as we construct a dictionary
-         containing both the forward and the reverse map. Hence, the provided map needs to be bijective.
-         It is also possible to specify labels for nested variables using "::", e.g. "a::b" corresponds
-         to the variable b within a
+        :param f: The CasADi function describing the dynamics. The output order should match the list provided
+         in the dot function.
+        :param names_map_in: A dict describing how to switch from the input names to those used in the function.
+         The key is the name provided by the user, while the value is the input name expected by the function.
+         It is also possible to specify labels for nested variables using ".", e.g. "a.b" corresponds
+         to the variable "b" within "a". This is valid only for the keys.
+         If time is an input, its label needs to be provided using the "dot" function.
         :return: Nothing
         """
         self._f = f
-        inv_map = {v: k for k, v in names_map_in.items()}  # inverse dict
-        self._names_map = names_map_in | inv_map  # merging forward and inverse map
+        self._names_map = names_map_in
+        self._names_map_inv = {v: k for k, v in names_map_in.items()}  # inverse dict
 
-    def function(self) -> cs.Function:
-        return self._f
+    def evaluate(
+        self, variables: Dict[str, cs.MX], time: cs.MX, time_name: str
+    ) -> Dict[str, cs.MX]:
+        input_dict = {}
+
+        for name in self.input_names():
+            if input not in variables:
+                raise ValueError(name + " input not found in variables.")
+            key = name if name not in self._names_map else self._names_map[name]
+            input_dict[key] = variables[name]
+
+        input_dict[time_name] = time
+
+        return self._f(input_dict)
 
     def input_names(self) -> List[str]:
         function_inputs = self._f.name_in()
         output = []
         for el in function_inputs:
-            output_name = self._names_map[el] if el in self._names_map else el
+            output_name = self._names_map_inv[el] if el in self._names_map_inv else el
             output.append(output_name)
 
         return output
 
-    def number_of_outputs(self) -> int:
-        return len(self._f.name_out())
+    def outputs(self) -> List[str]:
+        return self._f.name_out()
 
 
 @dataclasses.dataclass
 class DynamicsLHS:
     _x: List[str] = dataclasses.field(default=None)
     x: dataclasses.InitVar[List[str] | str] = None
-    _t: TTime = dataclasses.field(default=None)
     _t_label: str = "t"
+    t_label: dataclasses.InitVar[str] = None
 
-    def __post_init__(self, x: List[str] | str):
+    def __post_init__(self, x: List[str] | str, t_label: str):
         """
         Constructs the DynamicsLHS object
         :param x: List of variable names on the left hand side of dot{x} = f(y).
           The list can contain empty strings if some output of f needs to be discarded. If one output
-          needs to be mapped to a nested item, use "::" as separator, e.g. "a::b"
+          needs to be mapped to a nested item, use "." as separator, e.g. "a.b"
+        :param t_label: The label of the time variable. Default "t"
         :return: Nothing
         """
         self._x = x if isinstance(x, list) else [x]
-
-    def dt(self, t: TTime, label: str = "t") -> TDynamicsLHS:
-        self._t = t
-        self._t_label = label
-        return self
+        self._t_label = t_label if isinstance(t_label, str) else "t"
 
     def equal(self, f: cs.Function, names_map: Dict[str, str] = None) -> TDynamics:
         rhs = DynamicsRHS(f, names_map)
-        if rhs.number_of_outputs() != len(self._x):
+        if len(rhs.outputs()) != len(self._x):
             raise ValueError(
                 "The number of outputs of the dynamics function does not match the specified number of state variables."
             )
         return TypedDynamics(lhs=self, rhs=rhs)
 
-    def __eq__(self, other: cs.Function) -> TDynamics:
+    def __eq__(
+        self, other: cs.Function | Tuple[cs.Function, Dict[str, str]]
+    ) -> TDynamics:
+        if isinstance(other, Tuple):
+            return self.equal(f=other[0], names_map=other[1])
+
+        assert isinstance(other, cs.Function)
         return self.equal(f=other)
 
     def state_variables(self) -> List[str]:
@@ -93,8 +103,8 @@ class DynamicsLHS:
         return self._t_label
 
 
-def d(x: str | List[str]) -> TDynamicsLHS:
-    return DynamicsLHS(x)
+def dot(x: str | List[str], t: str = "t") -> TDynamicsLHS:
+    return DynamicsLHS(x, t_label=t)
 
 
 class Dynamics(abc.ABC):
@@ -127,7 +137,7 @@ class TypedDynamics(Dynamics):
         self._rhs = rhs
 
     def state_variables(self) -> List[str]:
-        return self._lhs.state_variables()
+        return [x for x in self._lhs.state_variables() if x]  # Remove empty strings
 
     def input_names(self) -> List[str]:
         return self._rhs.input_names()
@@ -136,7 +146,17 @@ class TypedDynamics(Dynamics):
         return self._lhs.time_label()
 
     def evaluate(self, variables: Dict[str, cs.MX], time: cs.MX) -> Dict[str, cs.MX]:
-        input_dict = variables
-        input_dict[self.time_name()] = time
+        f_output = self._rhs.evaluate(
+            variables=variables, time=time, time_name=self._lhs.time_label()
+        )
 
-        return self._rhs.function()(input_dict)
+        assert len(f_output) == len(self._rhs.outputs()) == self._lhs.state_variables()
+
+        output_dict = {}
+
+        for i in range(len(f_output)):
+            key = self._lhs.state_variables()[i]
+            if key:
+                output_dict[key] = f_output[self._rhs.outputs()[i]]
+
+        return output_dict
