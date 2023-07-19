b3a2ffe6a4ee106b0e583eb792f0d61e4da3ff30
Stefano <stefano.dafarra@gmail.com>
2023-07-19 10:11:36 +0200

    Added centroidal dynamics from point forces

---
+++ src/hippopt/robot_planning/dynamics/centroidal.py
@@ -0,0 +1,51 @@
+import casadi as cs
+
+
+def centroidal_dynamics_with_point_forces(
+    number_of_points: int,
+    mass_name: str = "m",
+    gravity_name: str = "g",
+    com_name: str = "com",
+    point_position_name: str = "p_#",
+    point_force_name: str = "f_#",
+    options: dict = None,
+) -> cs.Function:
+    if options is None:
+        options = dict()
+
+    input_vars = []
+
+    m = cs.MX.sym(mass_name, 1)
+    input_vars.append(m)
+
+    g = cs.MX.sym(gravity_name, 6)
+    input_vars.append(g)
+
+    x = cs.MX.sym(com_name, 3)
+    input_vars.append(x)
+
+    p = []
+    f = []
+    for i in range(number_of_points):
+        p.append(cs.MX.sym(point_position_name.replace("#", str(i)), 3))
+        input_vars.append(p[i])
+        f.append(cs.MX.sym(point_force_name.replace("#", str(i)), 3))
+        input_vars.append(f[i])
+
+    input_names = []
+    for var in input_vars:
+        input_names.append(var.name())
+
+    h_g = m @ g
+
+    for i in range(number_of_points):
+        h_g = h_g + cs.vertcat(f[i], cs.cross(p[i] - x, f[i]))
+
+    return cs.Function(
+        "centroidal_dynamics_with_point_forces",
+        input_vars,
+        [h_g],
+        input_names,
+        ["h_g"],
+        options,
+    )
