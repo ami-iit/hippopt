ebc9785767b91bdf4f05e95c335caf008189ff79
Stefano <stefano.dafarra@gmail.com>
2023-08-07 12:36:39 +0200

    Added initial conditions in contact variables.
    Removed u_v

--- src/hippopt/robot_planning/variables/contacts.py
+++ src/hippopt/robot_planning/variables/contacts.py
@@ -7,6 +7,7 @@ from hippopt import (
     Parameter,
     StorageType,
     Variable,
+    default_composite_field,
     default_storage_field,
 )
 
@@ -23,7 +24,7 @@ class ContactPointDescriptor(OptimizationObject):
 
     def __post_init__(
         self, input_foot_frame: str, input_position_in_foot_frame: np.ndarray
-    ):
+    ) -> None:
         self.foot_frame = input_foot_frame
         self.position_in_foot_frame = input_position_in_foot_frame
 
@@ -32,21 +33,26 @@ class ContactPointDescriptor(OptimizationObject):
 class ContactPoint(OptimizationObject):
     p: StorageType = default_storage_field(Variable)
     v: StorageType = default_storage_field(Variable)
-    u_v: StorageType = default_storage_field(Variable)
     f: StorageType = default_storage_field(Variable)
     f_dot: StorageType = default_storage_field(Variable)
 
-    descriptor: ContactPointDescriptor = dataclasses.field(default=None)
+    # Initial conditions
+    p0: StorageType = default_storage_field(Parameter)
+    v0: StorageType = default_storage_field(Parameter)
+
+    descriptor: ContactPointDescriptor = default_composite_field(time_varying=False)
 
     input_descriptor: dataclasses.InitVar[ContactPointDescriptor] = dataclasses.field(
         default=None
     )
 
-    def __post_init__(self, input_descriptor: ContactPointDescriptor):
+    def __post_init__(self, input_descriptor: ContactPointDescriptor) -> None:
         self.p = np.zeros(3)
         self.v = np.zeros(3)
-        self.u_v = np.zeros(3)
         self.f = np.zeros(3)
         self.f_dot = np.zeros(3)
+
+        self.p0 = np.zeros(3)
+        self.v0 = np.zeros(3)
 
         self.descriptor = input_descriptor
