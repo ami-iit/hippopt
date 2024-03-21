2ff833dfa1825d420fdcf7846397fde54b3e50b2
Stefano <stefano.dafarra@gmail.com>
2024-03-21 13:16:42 +0100

    Added conversion of an optimization object to a dictionary

--- test/test_opti_generate_objects.py
+++ test/test_opti_generate_objects.py
@@ -77,6 +77,26 @@ def test_generate_objects():
     assert (len(opti_var.to_list())) == len(test_var_as_list)
     expected_len = 7 + 3 * 7 + 3
     assert opti_var.to_mx().shape == (expected_len, 1)
+    as_dict = opti_var.to_dict()
+    assert all(
+        expected in as_dict
+        for expected in [
+            "aggregated.variable",
+            "aggregated.parameter",
+            "aggregated.scalar",
+            "other_parameter",
+            "aggregated_list[0].variable",
+            "aggregated_list[0].parameter",
+            "aggregated_list[0].scalar",
+            "aggregated_list[1].variable",
+            "aggregated_list[1].parameter",
+            "aggregated_list[1].scalar",
+            "aggregated_list[2].variable",
+            "aggregated_list[2].parameter",
+            "aggregated_list[2].scalar",
+        ]
+    )
+    assert "other" not in as_dict
 
 
 def test_generate_objects_list():
